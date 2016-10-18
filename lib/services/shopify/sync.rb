aa864a6ab77b7099f957e3cd3c94047122e19073
Ricardo Berdejo <ricardo.berdejo@koombea.com>
2016-10-18 16:54:08 -0500

    changed the way to get all products to bypass shopify 250 limit

--- lib/services/shopify/sync.rb
+++ lib/services/shopify/sync.rb
@@ -14,9 +14,14 @@ module Services
         api_password = Figaro.env.s_password
         store = Figaro.env.s_store
         shop_url = "https://#{api_key}:#{api_password}@#{store}.myshopify.com/admin"
+        response = HTTParty.get("#{shop_url}/products/count.json")
+        total_productos = JSON.parse(response.body)["count"]
         ShopifyAPI::Base.site = shop_url
-
-        products = ShopifyAPI::Product.find(:all)
+        pages = (total_productos / 250) + (total_productos % 250 > 0 ? 1 : 0)
+        products = []
+        pages.times do |page|
+          products.concat ShopifyAPI::Product.find(:all, :params => {limit: 250, page: page})
+        end
         @variants = products.collect { |p| p.variants }
         @variants = @variants.flatten
         @unkown_skus = []
@@ -33,17 +38,22 @@ module Services
             if variante.inventory_quantity != new_qty
               variante.inventory_quantity = new_qty.to_i
               if variante.save!
+                puts "updated #{sku}"
                 @updated.push(sku)
               else
+                puts "error #{sku}"
                 @errors.push(sku)
               end
             else
+              puts "up to date #{sku}"
               @up_to_date.push(sku)
             end
           else
+            puts "out_of_track #{sku}"
             @out_of_track.push(sku)
           end
         else
+          puts "unkown_skus #{sku}"
           @unkown_skus.push(sku)
         end
       end
